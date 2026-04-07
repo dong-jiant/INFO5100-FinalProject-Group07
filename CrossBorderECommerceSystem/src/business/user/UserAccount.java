f8994d6b16037092538b3ab0c687ccfe31b624a6
dong-jiant <dong.jiant@northeastern.edu>
2026-04-06 22:31:24 -0700

    Add UserAccount class

---
+++ CrossBorderECommerceSystem/src/business/user/UserAccount.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package business.user;
+
+/**
+ *
+ * @author stelladong
+ */
+public class UserAccount {
+    private String username;
+    private String password;
+
+    public UserAccount(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
