218feedaee2687b494fbbd8bf683a2131d04e685
dong-jiant <dong.jiant@northeastern.edu>
2026-04-06 22:57:18 -0700

    Add Order class with total price calculation

---
+++ CrossBorderECommerceSystem/src/business/order/Order.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package business.order;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author stelladong
+ */
+public class Order {
+    private ArrayList<OrderItem> items;
+
+    public Order() {
+        this.items = new ArrayList<>();
+    }
+
+    public void addItem(OrderItem item) {
+        items.add(item);
+    }
+
+    public ArrayList<OrderItem> getItems() {
+        return items;
+    }
+
+    public double getTotalPrice() {
+        double total = 0;
+        for (OrderItem item : items) {
+            total += item.getTotal();
+        }
+        return total;
+    }
+    
+}
