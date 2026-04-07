2dae20528cdda0d695b6a1e8e6cf036332a74c05
dong-jiant <dong.jiant@northeastern.edu>
2026-04-06 22:18:03 -0700

    Add Organization class and link to Enterprise; create additional business packages

---
+++ CrossBorderECommerceSystem/src/business/organization/Organization.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package business.organization;
+
+/**
+ *
+ * @author stelladong
+ */
+public class Organization {
+    private String name;
+
+    public Organization(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+}
