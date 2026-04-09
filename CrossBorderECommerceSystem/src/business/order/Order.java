b55efa0b35019386c023c19e24df157c020fe72a
dong-jiant <dong.jiant@northeastern.edu>
2026-04-08 20:33:28 -0700

    implemented order management flow with detail view, table population, and CardLayout navigation

--- CrossBorderECommerceSystem/src/business/order/Order.java
+++ CrossBorderECommerceSystem/src/business/order/Order.java
@@ -12,6 +12,8 @@ import java.util.ArrayList;
  */
 public class Order {
     private ArrayList<OrderItem> items;
+    private String orderId;
+    private String status;
 
     public Order() {
         this.items = new ArrayList<>();
@@ -32,5 +34,24 @@ public class Order {
         }
         return total;
     }
+    public String getOrderId() {
+    return orderId;
+}
+
+public void setOrderId(String orderId) {
+    this.orderId = orderId;
+}
+
+public String getStatus() {
+    return status;
+}
+
+public void setStatus(String status) {
+    this.status = status;
+}
+@Override
+public String toString() {
+    return orderId;
+}
     
 }
